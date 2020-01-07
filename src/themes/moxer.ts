cae2965a47028bea6d620f4172a1accbc4e2c4cd
Mattia Astorino <astorino.mattia@gmail.com>
2020-01-07 20:00:54 +0100

    Configure eslint

--- src/themes/moxer.ts
+++ src/themes/moxer.ts
@@ -1,31 +1,31 @@
 export default {
-	name: 'Moxer',
-	type: 'dark',
-	ui: {
-		accent: '#fff017',
-		background: '#090A0F',
-		foreground: '#8E95B4',
-		borders: '#090A0F',
-		shade1: '#212431',
-		shade2: '#2C303F',
-		shade3: '#35394B',
-		shade4: '#3F445A',
-		shade5: '#555B77',
-		shade6: '#6B7394'
-	},
-	base: {
-		white: '#ffffff',
-		black: '#000000',
-		red: '#E75A5A',
-		orange: '#E19235',
-		yellow: '#FEE17C',
-		green: '#A9EFA3',
-		cyan: '#70E3EB',
-		blue: '#4C71F6',
-		paleblue: '#69A7D3',
-		purple: '#5654BC',
-		brown: '#AB633B',
-		pink: '#E382BC',
-		violet: '#A18EEF'
-	}
+  name: 'Moxer',
+  type: 'dark',
+  ui: {
+    accent: '#fff017',
+    background: '#090A0F',
+    foreground: '#8E95B4',
+    borders: '#090A0F',
+    shade1: '#212431',
+    shade2: '#2C303F',
+    shade3: '#35394B',
+    shade4: '#3F445A',
+    shade5: '#555B77',
+    shade6: '#6B7394'
+  },
+  base: {
+    white: '#ffffff',
+    black: '#000000',
+    red: '#E75A5A',
+    orange: '#E19235',
+    yellow: '#FEE17C',
+    green: '#A9EFA3',
+    cyan: '#70E3EB',
+    blue: '#4C71F6',
+    paleblue: '#69A7D3',
+    purple: '#5654BC',
+    brown: '#AB633B',
+    pink: '#E382BC',
+    violet: '#A18EEF'
+  }
 };
